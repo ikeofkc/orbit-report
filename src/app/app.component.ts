988f144f0c1fc421cbfb3a63cd1e0541519ae8b8
Ike Ah-Loe <ike@pop-os.localdomain>
2020-09-24 05:28:19 -0500

    Adding steps 1-5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Satellite } from './satellite';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'orbit-report';
+  sourceList: Satellite[];
+  constructor() {
+   this.sourceList = [];
+   let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+
+   window.fetch(satellitesUrl).then(function(response) {
+      response.json().then(function(data) {
+
+        // TODO: loop over satellites
+         let satData = data.satellites;
+         for (let i=0; i< satData.length; i++) {
+           // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+           let satelliteObj = new Satellite(satData[i].name, satData[i].type, satData[i].launchDate, satData[i].orbitType, satData[i].operational);
+           // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
+           this.sourceList.push(satelliteObj);
+          }
+
+      }.bind(this));
+   }.bind(this));
+
+}
 }
