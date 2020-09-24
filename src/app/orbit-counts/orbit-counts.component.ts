2bd30cd0e75f1ee365c2fe570dda673e82a257d2
Ike Ah-Loe <ike@pop-os.localdomain>
2020-09-24 08:56:53 -0500

    Updated favicon;completed all but last bonus mission. Don't understand last bonus mission's instructions.

--- src/app/orbit-counts/orbit-counts.component.ts
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -15,8 +15,8 @@ export class OrbitCountsComponent implements OnInit {
   }
 
   getTotal(category: string): number {
-    let tally = 0;
-    for (let i = 0; i < this.satellites.length; i++) {
+    let tally: number = 0;
+    for (let i: number = 0; i < this.satellites.length; i++) {
       if (this.satellites[i]["type"].toLowerCase() === category.toLowerCase()) {
         tally ++;
       }
